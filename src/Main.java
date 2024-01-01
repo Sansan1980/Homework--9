72866fed8d2a2e8c64be36b316a71bcc838fc9cf
Sansan1980 <152459976+Sansan1980@users.noreply.github.com>
2024-01-01 10:20:01 +0300

    Initiall commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,27 @@
 import java.sql.SQLOutput;
 
 public class Main {
+    public static void main(String[] args) {
+        task1();
 
 
     }
+
+    // Объявить метод «сгенерироватьМассив»
+    //generateRandomArray()
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100000) + 100_000;
+
+        }
+        return arr;
+
+    }
+
+
+    public static void task1() {
+        int[] arr = generateRandomArray();
+
 }
